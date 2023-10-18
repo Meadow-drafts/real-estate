cd12ba39c91dba7da61c11376d35e1eab7548598
Shirley Laurette <Shirleylaurette1@gmail.com>
2023-10-18 15:54:24 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="bg-primary w-full text-5xl text-white overflow-hidden">
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
